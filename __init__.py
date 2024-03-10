a16f4ad864f913e5c37bb3f17bb91ba0f8c4e071
vanous <petr@linuks.cz>
2024-03-10 21:42:11 +0100

    Release 1.3.2

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "DMX",
     "description": "DMX visualization and programming, with GDTF/MVR and Network support",
     "author": "open-stage",
-    "version": (1, 3, 1),
+    "version": (1, 3, 2),
     "blender": (3, 4, 0),
     "location": "3D View > DMX",
     "doc_url": "https://blenderdmx.eu/docs/faq/",
