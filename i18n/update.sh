beaef56241230b6bc236af74cdfdcea855462cb4
vanous <petr@linuks.cz>
2024-05-04 07:20:56 +0200

    Update translation strings, remove file line numbers

--- i18n/update.sh
+++ i18n/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 #
-pybabel extract -F babel.cfg -o messages.pot ../
+pybabel extract -F babel.cfg --no-location -o messages.pot ../
 pybabel update -i messages.pot -d translations
 # pybabel compile -d translations # DO NOT RUN, creates binary merge weblate conflicts
