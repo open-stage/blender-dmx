46ccc27f2c88946eb510fffd16c5101a56002ea2
Sebastian <61376632+nrgsille76@users.noreply.github.com>
2024-07-01 15:41:24 +0200

    mvr: Use load_3ds definition to parse filepath
    Use load_3ds definition to parse filepath because the collection importer needs a file object

--- mvr.py
+++ mvr.py
@@ -19,7 +19,7 @@
 import os
 import bpy
 from .util import xyY2rgbaa
-from .io_scene_3ds.import_3ds import load
+from .io_scene_3ds.import_3ds import load_3ds
 from mathutils import Matrix
 import time
 import hashlib
@@ -250,7 +250,7 @@ def loadModelAndPrepareMvrFileCollection(file, folder):
     if file_name.split(".")[-1] == "glb":
         bpy.ops.import_scene.gltf(filepath=file_name)
     else:
-        load(None, bpy.context, folder, file)
+        load_3ds(file_name, bpy.context, CONSTRAIN=0.01, FILTER={'MESH'})
         file_3ds = True
     objs = list(bpy.context.view_layer.objects.selected)
 
