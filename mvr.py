8ed70fa43012b193abd4273e99e368e5cb681de2
Sebastian <61376632+nrgsille76@users.noreply.github.com>
2024-07-22 13:42:15 +0200

    mvr: Removed appending object to dict if already existing

--- mvr.py
+++ mvr.py
@@ -143,12 +143,13 @@ def process_mvr_object(context, mvr_scene, mvr_object, mvr_index, mscale, extrac
         print("adding %s... %s" % (node_type, mesh_name))
         if len(ob_exist):
             new_object = next((ob for ob in ob_exist), False)
+            if new_object:
+                imported_objects.append(new_object)
         elif len(mesh_exist) and not new_object:
             for mesh in mesh_exist:
                 mesh_id = mesh.get('MVR Name')
                 new_object = object_data.new(mesh_id, mesh)
             if new_object:
-                objectData.setdefault(uid, []).append(new_object)
                 imported_objects.append(new_object)
         elif not new_object:
             file_name = os.path.join(folder, file)
