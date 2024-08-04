c13d9106beed478c3af1cd4f6ad3d51ce3f2f8a8
vanous <petr@linuks.cz>
2024-08-04 18:36:53 +0200

    Small test script changes

--- scripts/test_fixture_patching.py
+++ scripts/test_fixture_patching.py
@@ -1,9 +1,10 @@
-#run this way:
+# run this way:
 # blender --background --python ./test_fixture_patching.py
 
 import bpy
 import os
 from mathutils import Vector
+from types import SimpleNamespace as SN
 
 addon_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 os.chdir(addon_path)
@@ -12,6 +13,8 @@ profiles_path = os.path.join(addon_path, "assets", "profiles")
 bpy.context.scene.dmx.new()
 # from dmx.gdtf import DMX_GDTF
 from dmx.scripts.get_testing_gdtfs import fixture_files
+
+# fixture_files = [SN(name="")]
 
 test_only = ["Cameo@Evos_W7@Firmware-1.4_GDTF-1.2.gdtf", "Ayrton@Argo_6_FX@V1.1_First_Release.gdtf"]
 test_only = []
@@ -26,7 +29,7 @@ def process_children(o):
 for fixture_file in fixture_files:
     print("Adding", fixture_file.name)
     f = bpy.context.scene.dmx.fixtures.add()
-    f.build(f"{fixture_file.name}", fixture_file.name, "default", 1, 1, (0, 0, 0), True, True)
+    f.build(f"test", fixture_file.name, "default", 1, 1, (0, 0, 0), True, True)
     for obj in f.objects:
         print(obj.name, obj.object.dimensions)
         process_children(obj.object.children)
