4b8b8177e4b3042fd22a2e2ac64669a43ec0860f
vanous <petr@linuks.cz>
2024-09-07 16:54:56 +0200

    Change description

--- dmx.py
+++ dmx.py
@@ -336,8 +336,8 @@ class DMX(PropertyGroup):
         type = tracker.DMX_Tracker)
 
     trackers_i : IntProperty(
-        name = _("Trackers Item"),
-        description=_("The selected element on the trackers list"),
+        name = _("Selected PSN Server"),
+        description=_("The selected element on the PSN server list"),
         default = 0
         )
 
