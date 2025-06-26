5c9188a6b4bb6fad2a2d364365427651fd1663e9
vanous <petr@linuks.cz>
2025-06-26 21:18:07 +0200

    Revert "Update translations" to allow weblate merging
    This reverts commit 7b5284420510a7fcb55a015ae2a29b8d15a0a1f1.

--- i18n/update.sh
+++ i18n/update.sh
@@ -18,6 +18,6 @@
 
 #
 #
-uv run pybabel extract -F babel.cfg --no-location -o messages.pot ../
-uv run pybabel update -i messages.pot -d translations
+pybabel extract -F babel.cfg --no-location -o messages.pot ../
+pybabel update -i messages.pot -d translations
 # pybabel compile -d translations # DO NOT RUN, creates binary merge weblate conflicts
